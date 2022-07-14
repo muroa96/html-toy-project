a39ef839ab7dd7fe747527b39d5910b6732ecab4
muroa96 <thsgskwhd@gmail.com>
2022-07-14 09:50:52 +0900

    first commit

---
+++ Calculator/test.js
@@ -0,0 +1,9 @@
+function SayHello(name,age){
+  console.log('Hi my name is ${name} and my age is ${age}');
+  }
+SayHello('한종', 27);
+
+function sayHello(name,age){  
+  console.log(`Hello ${name} you are ${age} years old`);  //백틱> 스트링에 %{}사용
+  }
+sayHello("Cecy", 28); 
